177504e967f9ade4131eb42d23dd537f78466a6a
anon <anon@anon.anon>
2026-07-04 15:14:17 +0200

    bump criterion tests

--- test/test-criterion.c
+++ test/test-criterion.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 constexpr size_t N_TEST_IMAGES = 9;
 struct test_image_t {
@@ -82,6 +83,16 @@ struct test_image_t {
     },
 };
 
+// -----------------------------
+// -----------------------------
+//  _  _     _
+// | || |___| |_ __  ___ _ _ ___
+// | __ / -_) | '_ \/ -_) '_(_-<
+// |_||_\___|_| .__/\___|_| /__/
+//            |_|
+// -----------------------------
+// -----------------------------
+
 static
 int ints_per_pixel(pnm_type_t type) {
     return (type == PNM_PIX_ASCII || type == PNM_PIX_BINARY) ? 3 : 1;
@@ -91,6 +102,58 @@ static
 void crex_assert_file_open(FILE * f, const char * s) {
     cr_assert_not_null(f, "Failed opening %s", s);
 }
+
+static
+void test_read_image_proto(struct test_image_t image) {
+    FILE * f = fopen(image.name, "r");
+    crex_assert_file_open(f, image.name);
+
+    int size = read_pnm_header(
+        f,
+        image.type,
+        NULL,
+        NULL,
+        NULL
+    );
+    cr_assert(size > 0);
+
+    int * buffer = malloc(size * sizeof(int));
+    cr_assert_not_null(buffer);
+
+    cr_assert(lt(int, 0, read_pnm_data(
+        f,
+        image.type,
+        buffer
+    )));
+
+    free(buffer);
+}
+
+static
+void test_write_image_proto(pnm_type_t type, int * b) {
+    FILE * f = tmpfile();
+    crex_assert_file_open(f, "tmpfile");
+
+    cr_assert(lt(int, 0, write_pnm_file(
+        f,
+        type,
+        b,
+        4,
+        4,
+        255
+    )));
+
+    fclose(f);
+}
+
+// -------------------------------
+// -------------------------------
+//  _  _             _
+// | || |___ __ _ __| |___ _ _ ___
+// | __ / -_) _` / _` / -_) '_(_-<
+// |_||_\___\__,_\__,_\___|_| /__/
+// -------------------------------
+// -------------------------------
 
 Test(plumblism, header_type_correctness) {
     for (size_t i = 0; i < N_TEST_IMAGES; ++i) {
@@ -120,7 +183,8 @@ Test(plumblism, header_storage_requirement) {
                      * ints_per_pixel(test_images[i].type)
         ;
 
-        int actual = read_pnm_header(f,
+        int actual = read_pnm_header(
+            f,
             test_images[i].type,
             NULL,
             NULL,
@@ -138,77 +202,123 @@ Test(plumblism, header_storage_requirement) {
     }
 }
 
-Test(plumblism, write_3x3_images) {
-    static const pnm_type_t formats[] = {
-        PNM_BIT_ASCII,
-        PNM_GRE_ASCII,
-        PNM_PIX_ASCII,
-        PNM_BIT_BINARY,
-        PNM_GRE_BINARY,
-        PNM_PIX_BINARY,
-    };
+// -------------------------------
+// -------------------------------
+//  ___             _ _
+// | _ \___ __ _ __| (_)_ _  __ _
+// |   / -_) _` / _` | | ' \/ _` |
+// |_|_\___\__,_\__,_|_|_||_\__, |
+//                          |___/
+// -------------------------------
+// -------------------------------
 
-    for (size_t i = 0; i < sizeof(formats)/sizeof(formats[0]); ++i) {
-        pnm_type_t type = formats[i];
-
-        int pixels[27];
-
-        for (int j = 0; j < 27; ++j) {
-            pixels[j] = j & 1;
-        }
-
-        if (type == PNM_GRE_ASCII || type == PNM_GRE_BINARY) {
-            for (int j = 0; j < 9; ++j)
-                pixels[j] = j * 28;
-        }
-
-        if (type == PNM_PIX_ASCII || type == PNM_PIX_BINARY) {
-            for (int j = 0; j < 27; ++j)
-                pixels[j] = (j * 23) % 256;
-        }
-
-        FILE * f = tmpfile();
-        cr_assert_not_null(f);
-
-        cr_assert(0 <= write_pnm_file(
-            f,
-            type,
-            pixels,
-            3,
-            3,
-            255
-        ));
-
-        rewind(f);
-
-        int w, h, maxval;
-
-        int size = read_pnm_header(
-            f,
-            type,
-            &w,
-            &h,
-            &maxval
-        );
-
-        cr_expect(eq(int, w, 3));
-        cr_expect(eq(int, h, 3));
-
-        int buffer[27];
-
-        memset(buffer, 0, sizeof(buffer));
-
-        cr_assert(0 <= read_pnm_data(
-            f,
-            type,
-            buffer
-        ));
-
-        cr_expect_arr_eq(pixels, buffer, size);
-
-        fclose(f);
-    }
+Test(plumblism, read_image_pbm_hand_ascii) {
+    test_read_image_proto(test_images[0]);
 }
+
+Test(plumblism, read_image_pgm_hand_ascii) {
+    test_read_image_proto(test_images[1]);
+}
+
+Test(plumblism, read_image_ppm_hand_ascii) {
+    test_read_image_proto(test_images[2]);
+}
+
+Test(plumblism, read_image_pbm_gimp_ascii) {
+    test_read_image_proto(test_images[3]);
+}
+
+Test(plumblism, read_image_pgm_gimp_ascii) {
+    test_read_image_proto(test_images[4]);
+}
+
+Test(plumblism, read_image_ppm_gimp_ascii) {
+    test_read_image_proto(test_images[5]);
+}
+
+Test(plumblism, read_image_pbm_gimp_binary) {
+    test_read_image_proto(test_images[6]);
+}
+
+Test(plumblism, read_image_pgm_gimp_binary) {
+    test_read_image_proto(test_images[7]);
+}
+
+Test(plumblism, read_image_ppm_gimp_binary) {
+    test_read_image_proto(test_images[8]);
+}
+
+// -------------------------------
+// -------------------------------
+// __      __   _ _   _
+// \ \    / / _(_) |_(_)_ _  __ _
+//  \ \/\/ / '_| |  _| | ' \/ _` |
+//   \_/\_/|_| |_|\__|_|_||_\__, |
+//                          |___/
+// -------------------------------
+// -------------------------------
+
+Test(plumblism, write_image_pbm_ascii) {
+    int b[2];
+    b[0] = INT_MAX;
+    b[1] = INT_MAX;
+    test_write_image_proto(PNM_BIT_ASCII, b);
+}
+
+Test(plumblism, write_image_pbm_binary) {
+    int b[2];
+    b[0] = INT_MAX;
+    b[1] = INT_MAX;
+    test_write_image_proto(PNM_BIT_ASCII, b);
+}
+
+Test(plumblism, write_image_pgm_ascii) {
+    int b[16];
+    for (int j = 0; j < 16; ++j) {
+        b[j] = j * 3;
+    }
+    test_write_image_proto(PNM_GRE_ASCII, b);
+}
+
+Test(plumblism, write_image_pgm_binary) {
+    int b[16];
+    for (int j = 0; j < 16; ++j) {
+        b[j] = j * 3;
+    }
+    test_write_image_proto(PNM_GRE_BINARY, b);
+}
+
+Test(plumblism, write_image_ppm_ascii) {
+    int b[16*3];
+    for (int j = 0; j < 16*3; j += 3) {
+        int color = j * 3;
+        b[j+0] = color;
+        b[j+1] = color;
+        b[j+2] = color;
+    }
+    test_write_image_proto(PNM_PIX_ASCII, b);
+}
+
+Test(plumblism, write_image_ppm_binary) {
+    int b[16*3];
+    for (int j = 0; j < 16*3; j += 3) {
+        int color = j * 3;
+        b[j+0] = color;
+        b[j+1] = color;
+        b[j+2] = color;
+    }
+    test_write_image_proto(PNM_PIX_BINARY, b);
+}
+
+// -------------------------------
+// -------------------------------
+//   ___                _
+//  / __|___ _ __  _ __| |_____ __
+// | (__/ _ \ '  \| '_ \ / -_) \ /
+//  \___\___/_|_|_| .__/_\___/_\_|
+//                |_|
+// -------------------------------
+// -------------------------------
 
 Test(plumblism, read_write_read_roundtrip) {
     for (size_t i = 0; i < N_TEST_IMAGES; ++i) {
@@ -223,7 +333,7 @@ Test(plumblism, read_write_read_roundtrip) {
 
         do {
             FILE * in = fopen(test_images[i].name, "r");
-            cr_assert_not_null(in);
+            crex_assert_file_open(in, test_images[i].name);
 
             size = read_pnm_header(
                 in,
@@ -232,36 +342,34 @@ Test(plumblism, read_write_read_roundtrip) {
                 &h,
                 &maxval
             );
-
             cr_assert(size > 0);
 
             original = malloc(size * sizeof(int));
             copy     = malloc(size * sizeof(int));
-
             cr_assert_not_null(original);
             cr_assert_not_null(copy);
 
-            cr_assert(0 <= read_pnm_data(
+            cr_assert(lt(int, 0, read_pnm_data(
                 in,
                 test_images[i].type,
                 original
-            ));
+            )));
 
             fclose(in);
         } while (0);
 
         do {
             FILE * tmp = tmpfile();
-            cr_assert_not_null(tmp);
+            crex_assert_file_open(tmp, "tmpfile");
 
-            cr_assert(0 <= write_pnm_file(
+            cr_assert(lt(int, 0, write_pnm_file(
                 tmp,
                 test_images[i].type,
                 original,
                 w,
                 h,
                 maxval
-            ));
+            )));
 
             rewind(tmp);
 
@@ -273,11 +381,11 @@ Test(plumblism, read_write_read_roundtrip) {
                 &rmax
             );
 
-            cr_assert(0 <= read_pnm_data(
+            cr_assert(lt(int, 0, read_pnm_data(
                 tmp,
                 test_images[i].type,
                 copy
-            ));
+            )));
 
             fclose(tmp);
         } while (0);
